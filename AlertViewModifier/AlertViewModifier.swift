3f3761f7f0f69a4bba91b4fd1e912f52d26c15ad
Nang Nguyen <nangnguyendtu@gmail.com>
2024-10-06 18:51:10 +0700

    fix: hot fix

--- AlertViewModifier/AlertViewModifier.swift
+++ AlertViewModifier/AlertViewModifier.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct AlertViewModifier: ViewModifier {
-    @Binding var alert: Alert?
+    @Binding var alert: AlertObject?
     
     func body(content: Content) -> some View {
         content
@@ -30,21 +30,29 @@ struct AlertViewModifier: ViewModifier {
     }
 }
 
-extension View {
-    func onViewAlert(with alert: Binding<Alert?>) -> some View {
+public extension View {
+    func onViewAlert(with alert: Binding<AlertObject?>) -> some View {
         self.modifier(AlertViewModifier(alert: alert))
     }
 }
 
-struct Alert {
+public struct AlertObject {
     let title: String
     let message: String
     let titleSecondaryButton: String?
     let titlePrimaryButton: String?
     let alertType: AlertType
+    
+    public init(title: String, message: String, titleSecondaryButton: String? = nil, titlePrimaryButton: String? = nil, alertType: AlertType) {
+        self.title = title
+        self.message = message
+        self.titleSecondaryButton = titleSecondaryButton
+        self.titlePrimaryButton = titlePrimaryButton
+        self.alertType = alertType
+    }
 }
 
-enum AlertType {
+public enum AlertType {
     case alert, confirm, destructive
     var primaryButtonRole: ButtonRole? {
         switch self {
