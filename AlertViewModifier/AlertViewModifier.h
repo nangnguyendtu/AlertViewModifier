e30cc3fa7aa68c64b226e189b382bcca77db4433
Nang Nguyen <nangnguyendtu@gmail.com>
2024-10-06 15:38:17 +0700

    Initial Commit

---
+++ AlertViewModifier/AlertViewModifier.h
@@ -0,0 +1,18 @@
+//
+//  AlertViewModifier.h
+//  AlertViewModifier
+//
+//  Created by Mac on 06/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlertViewModifier.
+FOUNDATION_EXPORT double AlertViewModifierVersionNumber;
+
+//! Project version string for AlertViewModifier.
+FOUNDATION_EXPORT const unsigned char AlertViewModifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertViewModifier/PublicHeader.h>
+
+
